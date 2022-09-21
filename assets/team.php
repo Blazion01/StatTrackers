0bc2e564aeaf9788c03fb5440e78908cb4788607
Blazion01 <91496872+Blazion01@users.noreply.github.com>
2022-09-21 13:19:13 +0200

    added setGameResults

--- assets/team.php
+++ assets/team.php
@@ -1,6 +1,6 @@
 <?php require_once "pdo.php";
 
-function createTeam($name) {
+function createTeam(string $name) {
   $pdo = $GLOBALS["pdo"];
   $sql = "INSERT IGNORE INTO `team` (`name`)
           VALUES (?);";
@@ -8,7 +8,7 @@ function createTeam($name) {
   $stmt->execute([$name]);
 }
 
-function addMember($team, $user) {
+function addMember(int $team, int $user) {
   $pdo = $GLOBALS["pdo"];
   $sql = "INSERT INTO `mtm_user_team` (`user_id`, `team_id`)
           VALUES (?, ?)
@@ -17,11 +17,39 @@ function addMember($team, $user) {
   $stmt->execute([$user, $team]);
 }
 
-function removeMember($team, $user) {
+function removeMember(int $team, int $user) {
   $pdo = $GLOBALS["pdo"];
   $sql = "UPDATE `mtm_user_team` (`user_id`, `team_id`)
           SET `active` = false
           WHERE `user_id` = ? AND `team_id` = ?;";
   $stmt = $pdo->prepare($sql);
   $stmt->execute([$user, $team]);
+}
+
+function setGameResults(int $game, int $team, array $playerContributions) {
+  $pdo = $GLOBALS["pdo"];
+  $messages = $GLOBALS["messages"];
+
+  try {
+    $sql = "SELECT `user_id` FROM `mtm_user_team` WHERE `team_id` = $team AND `active` = true;";
+    $players = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($players as $key => $player) {
+      if(isset($playerContributions[$player])) {
+        $goals = $playerContributions[$player]["goals"];
+        $assists = $playerContributions[$player]["assists"];
+      } else {
+        $goals = 0;
+        $assists = 0;
+      }
+      $sql = "INSERT IGNORE INTO `goals` (`game_id`, `team_id`, `user_id`, `goals`, `assists`)
+          VALUES (?, ?, ?, ?, ?);";
+      $stmt = $pdo->prepare($sql);
+      $stmt->execute([$game, $team, $player, $goals, $assists]);
+    }
+  } catch (Exception $e) {
+    $messages[count($messages)] = $e;
+    return;
+  }
+  $messages[count($messages)] = "Resultaten zijn erin gezet";
+  return;
 }
